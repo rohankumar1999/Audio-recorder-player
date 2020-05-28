06557029dfc819b4f86e42944166fd243a5d2e11
rohan <rohanbayya99@gmail.com>
2020-05-28 20:03:29 +0530

    UI & timer issue fixed

--- src/Record.js
+++ src/Record.js
@@ -1,68 +1,49 @@
-import React, {Component, useState} from "react";
-import "./App.css";
-import AudioAnalyser from "react-audio-analyser";
+import React, { useState } from 'react';
+import './audio.css';
+import AudioAnalyser from 'react-audio-analyser';
 import 'tachyons';
-const Demo=(props)=>{
-    // const[status,setStatus]=useState("");
-    const[audioType,setAudioType]=useState("audio/wav");
-    const[audioSrc,setAudioSrc]=useState("");
-    // const controlAudio=(status)=>{
-    //     setStatus(status)
-    //     // console.log(status)
-    // }
-    // const changeScheme=(e)=>{
-    //     setAudioType(e.target.value);
-    // }
-    return(
+
+const Demo = props => {
+    const [audioType, setAudioType] = useState('audio/wav');
+    const [audioSrc, setAudioSrc] = useState('');
+    return (
         <div className="record">
-                <AudioAnalyser
-                    status={props.status}
-                    audioType = {audioType}
-                    timeslice = {1000}
-                    audioSrc={audioSrc}
-                    className="center"
-                    startCallback={ (e) => {
-                        console.log("succ start", e)
-                    }}
+            <AudioAnalyser
+                backgroundColor={'#fff'}
+                size={'10%'}
+                // width={'100%'}
+                width={200}
+                strokeColor={'#000000'}
+                status={props.status}
+                audioType={audioType}
+                timeslice={1000}
+                audioSrc={audioSrc}
+                className="center"
+                startCallback={e => {
+                    console.log('succ start', e);
+                }}
 
-                    pauseCallback={ (e) => {
-                        console.log("succ pause", e)
-                    }}
-                    stopCallback={ (e) => {
-                       
-                        // console.log("succ stop", e)
-                        // window.URL.createObjectURL(e);
-                        // console.log('url is: ',window.URL.createObjectURL(e))
+                pauseCallback={e => {
+                    console.log('succ pause', e);
+                }}
+                stopCallback={e => {
+                    // console.log("succ stop", e)
+                    // window.URL.createObjectURL(e);
+                    // console.log('url is: ',window.URL.createObjectURL(e))
 
-                        // console.log('base64 encoded: ',window.btoa(window.URL.createObjectURL(e)))
+                    // console.log('base64 encoded: ',window.btoa(window.URL.createObjectURL(e)))
 
-                        {props.onCallFiles(window.URL.createObjectURL(e))}
-                        
-                    }}
-                    onRecordCallback={ (e) => {
-                        console.log("recording", e)
-                    }}
-                    errorCallback={ (err) => {
-                        console.log("error", err)
-                    }}
-                    />
-
-
-                
-                    
-                        
-                        
-                    
-                {/* </AudioAnalyser> */}
-                {/* <p >choose output type</p>
-                <select class="w-100 f6 h2 bg-white mb2 dib ma1 b--black-20" name="" id="" onChange={(e) => changeScheme(e)} value={audioType}>
-                     <option value="audio/mp3">audio/mp3</option>
-                    <option value="audio/webm">audio/webm（default）</option>
-                    <option value="audio/wav">audio/wav</option>
-                    
-                </select> */}
-            </div>
-    )
-}
+                    { props.onCallFiles(window.URL.createObjectURL(e)); }
+                }}
+                onRecordCallback={e => {
+                    console.log('recording', e);
+                }}
+                errorCallback={err => {
+                    console.log('error', err);
+                }}
+            />
+        </div >
+    );
+};
 
 export default Demo;
--- src/Timer.js
+++ src/Timer.js
@@ -1,132 +1,201 @@
 import React, { useState, useEffect } from 'react';
-import {AiFillPlaySquare,AiFillPauseCircle,AiFillCloseCircle} from 'react-icons/ai'
-import {FaStopCircle,FaMicrophoneAlt} from 'react-icons/fa'
-
+import { AiFillPlaySquare, AiFillPauseCircle, AiFillCloseCircle } from 'react-icons/ai';
+import { FaStopCircle, FaMicrophoneAlt } from 'react-icons/fa';
+import './audio.css';
 // import AudioAnalyser from "react-audio-analyser";
-import 'tachyons'
-import Demo from './Record'
-const Timer = () => {
-  const [seconds, setSeconds] = useState(3);
-  const [st, setSt] = useState("");
-  const [files,setFiles]=useState([])
-  const toggle=(st)=> {
-    setSt(st);
-  }
+import 'tachyons';
+import Demo from './Record';
 
-  const reset=() =>{
+const Timer = props => {
+  const maxRecordings = props && props.question && props.question.response_length ? props.question.response_length : 3;
+  const [seconds, setSeconds] = useState(3);
+  const [st, setSt] = useState('');
+  const [files, setFiles] = useState([]);
+  const toggle = st => {
+    setSt(st);
+  };
+
+  const reset = () => {
     setSeconds(3);
     // setSt("incactive");
-  }
-  
+  };
+
   useEffect(() => {
     let interval = null;
-    if(seconds===0){
-        toggle("inactive")
-        reset();
+    if (seconds === 0) {
+      setSeconds(0)
+      
+      toggle('inactive');
+      reset();
     }
-    if (st==="recording") {
+    if (st === 'recording') {
       interval = setInterval(() => {
-        
         setSeconds(seconds => seconds - 1);
-      }, 1000);
-    } else if (st==="paused" && seconds !== 3) {
+      }, 1030);
+    } else if (st === 'paused' && seconds !== 3) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   }, [st, seconds]);
-  
-const callFiles=(f)=>{
-    const list=[]
-    files.forEach((fi)=>{
-        list.push(fi);
-    })
-    list.push(f)
-    setFiles(list)
 
-}
+  const callFiles = f => {
+    const list = [];
+    files.forEach(fi => {
+      list.push(fi);
+    });
+    list.push(f);
+    setFiles(list);
+  };
 
-
-const triggerDelete=(f,index)=>{
+  const triggerDelete = (f, index) => {
     // alert('removing')
     // console.log(document.getElementById("app"))
     // document.getElementById(`file${index}`).style.display="none";
     // delete files[index]
-    
+
     // setFiles(delete files[index])
     // console.log('attempting to delete: ',ind)
-    if(window.confirm("Are you sure that you want to delete?")){
-        // var newList=[]
-        // newList=files.forEach((fi,i)=>{
-        //     if(i!==ind){
-        //         return fi;
-        //     }
-        // })
-        var it
-        var newList=[]
-        for(it=0;it<files.length;it++){
-                if(files[it]!==f){
-                    newList.push(files[it]);
-                }
+    if (window.confirm('Are you sure that you want to delete?')) {
+      // var newList=[]
+      // newList=files.forEach((fi,i)=>{
+      //     if(i!==ind){
+      //         return fi;
+      //     }
+      // })
+      let it;
+      const newList = [];
+      for (it = 0; it < files.length; it++) {
+        if (files[it] !== f) {
+          newList.push(files[it]);
         }
-        setFiles([...newList])
+      }
+      setFiles([...newList]);
     }
-    
-}
-const submitAudio=()=>{
-  files.forEach((f)=>{
-    console.log(window.btoa(f))
-  })
-  window.alert("Your Audio/s have been submitted!!")
-}
+  };
+
+  const submitAudio = () => {
+    // files.forEach(f => {
+    //   fetch(f)
+    //     .then((res) => {
+    //       res.blob().then(function (blob) {
+    //         var reader = new FileReader();
+    //         reader.addEventListener("loadend", function () {
+    //           var base64FileData = reader.result.toString();
+    //           var mediaFileObject = {
+    //             fileUrl: f,
+    //             size: blob.size,
+    //             type: blob.type,
+    //             src: base64FileData.slice(22)
+    //           };
+    //           console.log('audio file: ', mediaFileObject.src) //mediaFileObject.src is the actual encoded audio file
+    //         });
+
+    //         reader.readAsDataURL(blob);
+
+    //       });
+    //     });
+    // });
+    // window.alert('Your Audio/s have been submitted!!');
+  };
+  // const submitAudio = () => {
+  //   console.log('submitAudio');
+  //   props.onAnswerSelected();
+  //   // files.forEach((f) => {
+  //   //   console.log(window.btoa(f))
+  //   // })
+  //   // window.alert("Your Audio/s have been submitted!!")
+  // };
   return (
-    <div className="center h-5 w-50 br3  bg-light-blue pa3 dib" style={{width: '100%', height: '100%', backgroundColor: 'powderblue'}}>
-      <div className="ph3 pv2 mb2 dib w-50 center black " style={{ fontSize: '350%' ,display:'flex',justifyContent:'center'}}>
-        {seconds}s
-      </div>
-      
-      <div className="ph3 pv2 mb2 dib w-50 center black" style={{ fontSize: '250%' ,display:'flex',justifyContent:'center'}}>
-      <FaMicrophoneAlt size="80"/>
-      </div>
-      
-      <Demo status={st}  onCallFiles={callFiles}/>
+    <div style={{ width: '100%', height: '100%' }}>
+      <div className="ph3 pv2 mb2 dib w-50 center black " style={{ fontSize: '2em', display: 'flex', justifyContent: 'center' }}>
+        {seconds}
+        s
+                  </div>
 
+      <div className="ph3 pv2 mb2 dib w-50 center black" style={{ fontSize: '1em', display: 'flex', justifyContent: 'center' }}>
+        <FaMicrophoneAlt size="80" />
+      </div>
+      
+           <Demo status={st} onCallFiles={callFiles} width="400%"/>
       
       
-      <div className="row" style={{display:'flex',justifyContent:'space-between'}}>
-        
-        {st!=="recording"&&
-        <button id="start" className={` f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black button-primary-${st==="recording" ? 'active' : 'inactive'}`} onClick={()=>{toggle("recording")}}>
-        <AiFillPlaySquare size="40"/>
-        </button>}
-        {st==="recording"&&
-        <button id="pause" className={` f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black button-primary-${st==="recording" ? 'active' : 'inactive'}`} onClick={()=>{toggle("paused")}}>
-         <AiFillPauseCircle size="40"/> 
-        </button>}
-        
-        <button id="stop" className=" f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black"  onClick={()=>{toggle("inactive");
-        reset();
-    }}>
-        <FaStopCircle size="40"/>
+
+      <div className="row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+        {st !== 'recording'
+          && (
+            <button id="start"
+              style={{ background: "#B6B6B4", width: "5em", boxShadow: '0em 0.125em 0.25em #0000001A' }}
+              className={` f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black button-primary-${st === 'recording' ? 'active' : 'inactive'}`}
+              onClick={() => {
+                if (files && files.length >= maxRecordings) {
+                  alert(`You can not add more than ${maxRecordings} recordings.`);
+                  return;
+                }
+                toggle('recording');
+              }}>
+              <AiFillPlaySquare size="30" color="black" />
+            </button>
+          )}
+        {st === 'recording'
+          && (
+            <button id="pause"
+              style={{ background: "#B6B6B4", width: "5em", boxShadow: '0em 0.125em 0.25em #0000001A' }}
+              className={` f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black button-primary-${st === 'recording' ? 'active' : 'inactive'}`} onClick={() => { toggle('paused'); }}>
+              <AiFillPauseCircle size="30" color="black" />
+            </button>
+          )}
+        {(st==='paused'||st==='recording')
+        &&
+        <button
+          id="stop"
+          style={{ background: '#B6B6B4', width: '5em', boxShadow: '0em 0.125em 0.25em #0000001A' }}
+          className=" f6 link center br3 ph3 pv2 mb2 dib light-blue bg-black"
+          onClick={() => {
+            toggle('inactive');
+            reset();
+          }}
+        >
+          <FaStopCircle size="30" color="black" />
         </button>
+        }
+
       </div>
 
-      <div className="files pa3 center" style={{display:'flex',justifyContent:'center'}}>
-        <div style={{display:'flex',flexDirection:'column'}} >
-      {files.map((f,index)=><div class={`file${index}`} key={f.toString()} >
-          <button className="w-2 center f6 link grow  light-blue bg-black" style={{height:'2rem',width:'2rem',borderRadius:'60%'}} onClick={()=>{
-            //   alert('removing');
-            //   delete files[index]
-              triggerDelete(f,index)
-          }}><AiFillCloseCircle/></button>
-          <audio controls autoplay name="media"><source src={f} className="bg-red" type="audio/wav"></source></audio>
-          </div>
-      )}
+      <div className="files pa3 center" style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{
+          display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'
+        }}>
+          {files.map((f, index) => (
+            <div
+              style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', marginTop: '1em'
+              }}
+              className={`file${index}`}
+              key={f.toString()}>
+              <audio controls autoPlay name="media"><source src={f} className="bg-red" type="audio/wav" /></audio>
+              <div style={{ width: '2em' }} />
+              <button
+                className="w-2 center f6 link grow "
+                style={{ height: '2rem', width: '2rem', borderRadius: '60%' ,background: '#B6B6B4' }}
+                onClick={() => {
+                  //   alert('removing');
+                  //   delete files[index]
+                  triggerDelete(f, index)
+                }}
+              >
+                <AiFillCloseCircle size="15"/>
+
+              </button>
+
+            </div>
+          ))}
+        </div>
       </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {files.length > 0 ? <button className="submitButton" style={{ fontSize: '1.3em',background: '#B6B6B4',color:'black' ,fontFamily:'bold'}} onClick={submitAudio} type="button" >Submit</button> : null}
       </div>
-      <div style={{display:'flex',justifyContent:'center'}}>
-        {files.length>0?<button className=" w-20 center br4 f6 link grow ph3 pv2 mb2 dib white bg-black " style={{ fontSize: '150%'}} onClick={submitAudio} type="button">Submit</button>:null}
-      </div>
-      
+
     </div>
   );
 };
